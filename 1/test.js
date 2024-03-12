af67c500a0eb3d2255c75cfde48ba73fefdd5e5a
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-03-12 19:35:26 +0900

    240312 수업

---
+++ 1/test.js
@@ -0,0 +1,18 @@
+// let myName: string = "Alice";
+// const msg: string = 'hello';
+// const num: number = 123;
+// const bool: boolean = true;
+// function add(num1: number, num2: number) :number {
+//     return num1 + num2;
+// }
+// const plus = (num1: number, num2: number) :number => num1 + num2;
+// let age: number = 30;
+// let aaa: string = "Johb-n";
+// console.log(aaa.toUpperCase());
+// let nameObj = new String("John");
+function max(num1, num2) {
+    if (num1 == num2)
+        return num1;
+    return num2;
+}
+max(2, 3);
